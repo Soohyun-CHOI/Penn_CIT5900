350eb7ea0b56b2204bdab1063f2e8be7d050b0e9
Soohyun <csh0sns@naver.com>
2023-09-06 00:04:30 -0400

    chore: Add init file

---
+++ HW2_supermarket/HW2_init.py
@@ -0,0 +1,24 @@
+# import the random module
+# use "winnings = random.randint(2, 10)" to generate a random int from 2 - 10 and store in a variable "winnings"
+import random
+
+# unit price of a lottery ticket
+constant_lottery_unit_price = 2
+
+# unit price of an apple
+constant_apple_unit_price = .99
+
+# unit price of a can of beans
+constant_canned_beans_unit_price = 1.58
+
+# unit price of a soda
+constant_soda_unit_price = 1.23
+
+# the user has initial $5 for shopping
+money = 5
+
+# the user has spent $0 initially
+money_spent = 0
+
+# the amounts of lottery tickets, apples, cans of beans, and sodas the user has purchased
+lottery_amount, apple_amount, canned_beans_amount, soda_amount = 0, 0, 0, 0
